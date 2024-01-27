64e9a7ab10802add253747446f933b817010f707
109306035 <thomas2002015@gmail.com>
2024-01-27 22:33:00 +0800

    20240127 change meet commit

--- Assets/Scenes/UI/Scripts/ConsoletoText/DataFetcher.cs
+++ Assets/Scenes/UI/Scripts/ConsoletoText/DataFetcher.cs
@@ -4,8 +4,8 @@ using System.Collections;
 
 public class DataFetcher : MonoBehaviour
 {
-    private const string url = "http://192.168.0.141:8080/speech";
-    //字呈現在電腦(unity Game view)用127.0.0.1，呈現在hololens上用192.168.0.141
+    private const string url = "http://192.168.185.95:8080/speech";
+    //字呈現在電腦(unity Game view)用127.0.0.1，呈現在hololens上用192.168.0.141(實驗室) 更:不是0.174嗎?待試(不對 應該是看cmdserver畫面在哪裡跑)更:.185.95是小米
     private WaitForSeconds fetchInterval = new WaitForSeconds(1f);
 
     private IEnumerator Start()
